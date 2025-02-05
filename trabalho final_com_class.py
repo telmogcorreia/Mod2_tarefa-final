932ff6e1febd59793f72943496c756321e29b323
telmogcorreia <telmo_48@hotmail.com>
2025-02-05 20:24:19 +0000

    Update trabalho final_com_class.py
    Código ainda com mais comentários, e cabeçalho criado no ficheiro 'dinheiro.txt'

--- trabalho final_com_class.py
+++ trabalho final_com_class.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Jan 31 20:21:06 2025
+Created on Wed Feb  5 20:04:25 2025
 
 @author: telmo
 """
@@ -11,11 +11,15 @@ class GastoBase(ABC):  # Classe Abstrata vazia
     pass
 
 
-class Gasto(GastoBase):   # Classe derivada
+class Gasto(GastoBase):   # Classe Derivada
     def __init__(self):
         self.total_gastos = 0  # Total de gastos a zero, depois soma a quando cada gasto é inserido
-        open("dinheiro.txt", "w").close() # Abre e limpa o ficheiro "dinheiro.txt"
-                                          
+        with open("dinheiro.txt", "w") as f: # Abre e escreve o cabeçalho do fichiro "dinheiro.txt"
+            f.write("Aqui estão os seus gastos e categorias.\n")
+            f.write("\n")
+            f.write("\n")
+                    
+                                                  
     def registrar_gasto(self, valor, categoria):
         self.total_gastos += valor # Soma gasto ao total de gastos
         with open("dinheiro.txt", "a") as f: # Abre o ficheiro e escreve os novos gastos e categorias
@@ -33,17 +37,17 @@ print("\n")
 
 while True: 
     try:
-        gasto = float(input("Digite um gasto (ou '0' para finalizar): "))
-        if gasto == 0:
-            break # Quando digitar "0" Para o programa
-        elif gasto < 0:
-            print("Por favor, insira um valor positivo.") # Quando o numero é menor que 0 continua dentro do loop
+        valor = float(input("Digite um gasto (ou '0' para finalizar): "))
+        if valor == 0:
+            break # Quando digitar "0" Pará o programa
+        elif valor < 0:
+            print("Por favor, insira um valor positivo.") # Quando o numero é menor que '0' continua dentro do loop
         else:
             categoria = input("Digite a categoria do gasto: ") # Quando o valor é maior que zero, pergunta qual é a categoria do gasto
-            gasto_manager.registrar_gasto(gasto, categoria) # Chama o metodo registar_gasto para registar o gasto e a categoria.
+            gasto_manager.registrar_gasto(valor, categoria) # Chama o metodo registar_gasto para registar o gasto e a categoria.
     except ValueError:  # Caso não digite um numero aparece está mensagem
         print("Por favor, insira um número válido.")
 
 with open('dinheiro.txt', 'a') as f:  # Abre o ficheiro para escrever o total
-    f.write(f"\nTotal dos gastos: €{gasto_manager.total_gastos:.2f}\n") # Escreve o total no ficheiro
+    f.write(f"\nTotal dos gastos: {gasto_manager.total_gastos:.2f}€") # Escreve o total no ficheiro
 print(f"\nTotal dos gastos: {gasto_manager.total_gastos:.2f}€") # Escreve o total na consola do utilizador
